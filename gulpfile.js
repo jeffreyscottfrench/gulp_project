f04f36288ab19c7fe52df0e3dd63bdfeee7e4082
Jeffrey Scott French <jeffreyscottfrench@gmail.com>
2016-06-23 11:08:06 -0400

    Basic Gulp Workflow

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var runSequence = require('run-sequence');
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.+(scss|sass)')
+  .pipe(sass()) // Using gulp-sass
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+gulp.task('useref', function(){
+  return gulp.src('app/**/*.+(html|php)')
+  .pipe(useref())
+  .pipe(gulpIf('*.js', uglify()))
+  .pipe(gulpIf('*.css', cssnano()))
+  .pipe(gulp.dest('dist'))
+});
+gulp.task('images', function(){
+  return gulp.src('app./images/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('dist/images'))
+});
+gulp.task('fonts', function(){
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+gulp.task('clean:dist', function(){
+  return del.sync('dist');
+})
+gulp.task('cache:clear', function(){
+  return cache.clearAll(callback)
+});
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+});
+// gulp watch
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.+(scss|sass)', ['sass']);
+  gulp.watch('app/**/*.+(html|php)', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+gulp.task('build', function(callback){
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+gulp.task('default', function (callback){
+  runSequence(['sass', 'browserSync', 'watch'],
+  callback
+  )
+});
